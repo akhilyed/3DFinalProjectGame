f6370d5f9f0aa872daa9cd8fdf440beb81e8eb5d
akhilyed <158219372+akhilyed@users.noreply.github.com>
2024-06-11 23:33:36 -0400

    Initial commit

---
+++ world.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $AudioStreamPlayer2D.playing == false:
+		$AudioStreamPlayer2D.play()
+	pass
